4dec4fb09b9f692c1cef4337986d10294d03e9a9
Saurabh Zarapkar <sazarapkar@gmail.com>
2021-01-06 11:37:40 -0500

    added progress bar, time, and duration functionality

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+}
+
 function togglePlay(){
     if (video.paused) {
         video.play();
@@ -18,14 +23,36 @@ function togglePlay(){
         playBtn.setAttribute('title', 'Pause');
     } else {
         video.pause();
-        playBtn.classList.replace('fa-pause', 'fa-play');
-        playBtn.setAttribute('title', 'Play');
+        showPlayIcon();
     }
 }
 
+// On Video End, show play button
+video.addEventListener('ended', showPlayIcon);
+
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time/60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
 
+//Update progress bar as video plays
+function updateProgress() {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)}/`;
+    duration.textContent = `${displayTime(video.duration)}`;
+}
+
+//Click to seek within video
+function setProgress(e) {
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+}
 
 // Volume Controls --------------------------- //
 
@@ -41,3 +68,6 @@ function togglePlay(){
 // Event Listeners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
